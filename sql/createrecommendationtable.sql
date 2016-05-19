d497b355772bf861efd2d6fab42ac4867329b0bf
Alexander-Poganatz <Alexander-Poganatz@users.noreply.github.com>
2016-05-18 21:12:27 -0400

    Add files via upload

---
+++ sql/createrecommendationtable.sql
@@ -0,0 +1,7 @@
+--File author: Alexander Poganatz
+CREATE TABLE Recommendation
+(
+  recommendation_id INT NOT NULL AUTO_INCREMENT,
+  recommendation VARCHAR(20),
+  PRIMARY KEY(recommendation_id)
+);
---
+++ sql/createtypetable.sql
@@ -0,0 +1,7 @@
+--File author: Alexander Poganatz
+CREATE TABLE Beer_Type
+(
+  beer_type_id INT NOT NULL AUTO_INCREMENT,
+  beer_type VARCHAR(10) NOT NULL,
+  PRIMARY KEY(beer_type_id)
+);
