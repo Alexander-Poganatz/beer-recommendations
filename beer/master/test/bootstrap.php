860285833eaeb37dad47c00b8bf6cee9e6fa78e7
Alexander-Poganatz <Alexander-Poganatz@users.noreply.github.com>
2016-05-18 21:10:11 -0400

    Add files via upload

--- beer/master/test/bootstrap.php
+++ beer/master/test/bootstrap.php
@@ -1,1 +1,9 @@
-temp
+<?php
+  //File author: Alexander Poganatz
+  //Purpose: To include things for phpunit tests.
+
+  require_once "/home/alex/Documents/website/beer/master/utility.php";
+  require_once "/home/alex/Documents/website/beer/master/getallbeer.php";
+  require_once "/home/alex/Documents/website/beer/master/insertbeer.php";
+  require_once "/home/alex/Documents/website/beer/master/getbeer.php";
+ ?>
